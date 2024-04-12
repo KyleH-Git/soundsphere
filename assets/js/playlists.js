4326ef10905944829009451b6fe79388cff6ea7a
Kyle <dahaydnator@gmail.com>
2024-04-12 14:10:41 -0500

    finished adding songs to playlist functionality

--- assets/js/playlists.js
+++ assets/js/playlists.js
@@ -1,11 +1,13 @@
 //grab create new playlist button
 const newPlaylistEl = $('#new-list-btn');
 //grab current playlist display div
-const currentPlaylistEl = ('#current-list');
+let currentPlaylistEl = null;
 //grab currently saved playlist div
-const savedPlaylistEl = ('#saved-list');
+const savedPlaylistEl = $('#playlist-display');
 //grab playlist content display div
-const contentPlaylistEl = ('#content-list');
+const contentPlaylistEl = $('#content-list');
+const modalEl = $('#playlist-create');
+const songDisplayEl = $('#song-name');
 
 //get playlists from storage function, called on page load
 function getSavedPlaylist(){
@@ -26,8 +28,9 @@ function savePlaylist(playlist){
 }
 
 function selectPlaylist(){
-    currentPlaylistEl.on('click', '.playlist-card', function(event){
-        contentPlaylistEl = $(this.listDisplay);
+    savedPlaylistEl.on('click', '.playlist-card', function(event){
+        currentPlaylistEl = $(this).attr('data-playlist-id');
+        console.log(currentPlaylistEl);
     });
 }
 
@@ -37,15 +40,17 @@ function selectPlaylist(){
 //  song objects have artist name, album name, track name
 function createPlaylist(){
     const playlist = {
-        name: modalnameinput,
-        genre: modaldropdowninput,
-        created: dayjs('M-D-YYYY'), 
+        name: $('#playlist-name').val().trim(),
+        genre: $('#form-select').val(),
+        created: dayjs().format('M-D-YYYY'), 
         songs: []
     };
 
     //get currently saved playlists from storage
     const currentPlaylists = getSavedPlaylist();
     currentPlaylists.push(playlist);
+    savePlaylist(currentPlaylists);
+    displayPlaylist(currentPlaylists);
 }
 
 //display playlist function
@@ -53,9 +58,12 @@ function createPlaylist(){
 //  playlist div has title, genre, date created, and delete button displayed
 function displayPlaylist(){
     const playlists = getSavedPlaylist();
+    savedPlaylistEl.empty();
+
     playlists.forEach((list, i) => {
         const listDisplay = $('<div>');
         listDisplay.attr('class', 'playlist-card');
+        listDisplay.attr('data-playlist-id', i)
         const listName = $('<h3>');
         listName.text(list.name);
         const listGenre = $('<h4>');
@@ -77,19 +85,14 @@ function displayPlaylist(){
 function deletePlaylist(){
     const playlistID = $(this).attr('data-playlist-id');
     const playlists = getSavedPlaylist();
-
-    playlists.forEach((list, i)=> {
-        if(i === playlistID){
-            playlists.splice(i, 1);
-        }
-    });
-
+    playlists.splice(playlistID, 1);
     savePlaylist(playlists);
     displayPlaylist();
 }
 
 //display playlist content function - when a playlist is clicked, all current song objects in playlist are displayed
 function displaySong(playlist){ 
+    songDisplayEl.empty();
     playlist.songs.forEach(song => {
         const songCard = $('<div>');
         songCard.attr('class', 'card');
@@ -120,7 +123,7 @@ function displaySongs(){
 
     //if there are songs, loop through the array and find album name from stored title + artist
     if(songList !== null){
-        songList.forEach(song => {
+        songList.forEach((song, i)=> {
            
             const userSearch = song.title + " " + song.artist;
             let url = `https://genius-song-lyrics1.p.rapidapi.com/search/?q=${userSearch}&per_page=10&page=1`;
@@ -130,18 +133,20 @@ function displaySongs(){
                     'X-RapidAPI-Key': 'bdaf43f0c1mshb586ac38305d76bp1edea3jsnb94c9478d441',
                     'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
                 }
-            }
+            } 
+            setTimeout(() => {
+                
+        
                 fetch(url, options).then(function (response) {
                     if (response.ok) {
-                        response.json().then(function (data) {
+                        response.json().then(async function (data) {
                             const id = data.hits[0].result.id;
                             url = `https://genius-song-lyrics1.p.rapidapi.com/song/details/?id=${id}&text_format=plain`;
                             
-                        fetch(url, options).then(function (response) {
+                         fetch(url, options).then(function (response) {
                             if (response.ok) {
                                 response.json().then(function (data) {
                                     song.album = data.song.album.name;
-                                    const songDisplayEl = $('#song-name');
                                     const songCard = $('<div>');
                                     songCard.attr('class', 'card');
 
@@ -156,49 +161,85 @@ function displaySongs(){
                                  
                                     const addSong = $('<button>');
                                     addSong.text('Add Song');
-                                    addSong.on('click', function (){
-                                        if(currentPlaylistEl !== null){
-                                            currentPlaylistEl.push(song);
-                                        }
-                                    });
+                                    addSong.attr('data-song-id', i);
                                     songCard.append(songTitle, songArtist, songAlbum, addSong);
                                     songDisplayEl.append(songCard);
+                                    localStorage.setItem('songs', JSON.stringify(songList));
                                 });
                             }
                         });
                     });
                 }
             });
-            
-            
-            //create song html elements to be displayed in the song div and add them.
-            
+        }, 5000);           
         });     
         console.log(songList);
-        localStorage.setItem('songs', JSON.stringify(songList));
+        
     }
+}
+
+function getSavedSongs(){
+    const savedSongs = JSON.parse(localStorage.getItem('songs'));
+    if(savedSongs !== null){
+        return savedSongs;
+    }
+}
+
+function saveSongs(songs){
+    localStorage.setItem('songs', JSON.stringify(songs));
 }
 
 //add song function
 function addSong(){
-
+    //if there is a currently selected playlist
+    console.log(currentPlaylistEl);
+    if(currentPlaylistEl !== null){
+        //add the clicked song to the song element of that playlist
+        const savedSongs = getSavedSongs();
+        const songIndex = $(this).attr('data-song-id');
+        newSong = {
+            title: savedSongs[songIndex].title,
+            artist: savedSongs[songIndex].artist,
+            album: savedSongs[songIndex].album,
+        }
+        console.log(newSong);
+        const savedPlaylist = getSavedPlaylist();
+        savedPlaylist[currentPlaylistEl].songs.push(newSong);
+        savePlaylist(savedPlaylist);
+        //remove the clicked song from the displayed songs
+        deleteSong();
+    }
 }
 
 //delete song function
 function deleteSong(){
+    const songID = $(this).attr('data-song-id');
+    const songs = getSavedSongs();
+    songs.splice(songID, 1);
+    saveSongs(songs);
+    songDisplayEl.empty();
+    displaySongs();
+}
 
+function pageInit(){
+    displaySongs();
+    displayPlaylist();
 }
 
 //on page load, if songs have been selected from the main page display the songs from storage on the page
-window.onload = displaySongs;
-
+window.addEventListener("load", pageInit);
 
 //event listener for create playlist button
+modalEl.on('click', createPlaylist);
 
 //event listener for clicking on a saved playlist to display it as current playlist - attach to parent only activate if clicked 
+savedPlaylistEl.on('click', selectPlaylist);
+
+//event listener for adding song to playlist
+songDisplayEl.on('click', 'button', addSong);
+    
+
 
 //event listener for delete song from playlist - attach to parent only activate if clicked 
-
-//event listener for delete playlist from saved lists - attach to parent only activate if clicked 
 
 //event listener for lyrics button on current playlist songs - attach to parent only activate if clicked 
